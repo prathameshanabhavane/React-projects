cbb18141ca914b8f035d4c28e8284d0c07224ccf
Prathamesh Anabhavane <pratham291994@gmail.com>
2019-10-05 15:05:08 +0530

    removed logo import file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data : [],
+    }
+  }
+  
+  componentDidMount() {
+    fetch('https://facebook.github.io/react-native/movies.json').
+    then((Response) => Response.json()).
+    then((getresponse) => {
+      //console.log(getresponse.movies);
+      this.setState({
+        data: getresponse.movies,
+      })
+    })
+  }
+
+  render () {
+
+    const mystyle = {
+      color: "white",
+      backgroundColor: "DodgerBlue",
+      padding: "10px",
+      fontFamily: "Arial",
+      margin: "15px"
+    };
+
+    const headerstyle = {
+      color: "black",
+    }
+    const yearstyle = {
+      fontWeight: "700",
+      paddingBottom: "10px",
+      display: "block"
+    }
+
+    return (
+      <div className="App">
+        <div>
+          {this.state.data.map((dynamicdata,key) => 
+            <div key={key} style={mystyle}>
+                <h2 style={headerstyle}>
+                  {dynamicdata.title}
+                </h2>
+                <span style={yearstyle}>
+                  {dynamicdata.releaseYear} 
+                </span>
+            </div>
+            )}
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
